084e9e1c50f149597ecc13d49db6e1a746b66f05
Dawn Monroe <deetriplett@gmail.com>
2017-06-01 22:32:02 -0400

    Create units.php

---
+++ units.php
@@ -0,0 +1,12 @@
+<?php
+//number in pounds we want to convert to kilograms
+//floating point value for the pound to kilogram conversion
+//use the variables above to calculate pounds X kilograms conversion
+//display lbs to kilograms
+
+//number in miles we want to convert to kilometers
+//floating point value ffor the mile to kilometer conversion
+//use the variable above to calcute miles X kilometeres
+//display the miles to kilometers
+
+ ?>
